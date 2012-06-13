85ff78d408cb79b4ff147cddad29a57b565cf062
simanchala <simanchala@weboniselab.com>
2012-06-13 18:52:44 +0530

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,5 @@ Blow::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.precompile = %w{application.js}
 end
