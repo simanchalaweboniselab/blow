e7f9afc3e3d806c45b43603733df9aba83b83123
simanchala <simanchala@weboniselab.com>
2012-06-13 20:20:45 +0530

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Blow::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.assets.precompile = %w{application.js}
-  config.assets.precompile = %w{application.css}
-  config.assets.precompile = %w{active_admin.css.scss}
+  config.assets.precompile += %w[*.css,*.js]
+  #config.assets.precompile = %w{application.css}
+  #config.assets.precompile = %w{active_admin.css.scss}
 end
 
