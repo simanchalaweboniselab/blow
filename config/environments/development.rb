f53df41ab886168da26e684d53845c9a7b8b4219
simanchala <simanchala@weboniselab.com>
2012-06-14 18:15:21 +0530

    email configure3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,7 @@ Blow::Application.configure do
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
+      :domain         => 'heroku.com',
       :authentication => "plain",
       :enable_starttls_auto => true,
       :user_name => 'simanchala.pradhan',
