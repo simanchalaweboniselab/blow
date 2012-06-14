8c9e0106183dbb364534f38bc62b3969f2b2a4ff
simanchala <simanchala@weboniselab.com>
2012-06-14 18:39:50 +0530

    email configure6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,12 +70,12 @@ Blow::Application.configure do
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => 'heroku.com',
+      #:domain => 'heroku.com',
       :authentication => "plain",
       :enable_starttls_auto => true,
       :user_name => ENV["simanchala.pradhan"],
