fdc22574c89d9fba6d796cdee637cc52d4326005
simanchala <simanchala@weboniselab.com>
2012-06-20 17:56:29 +0530

    changes in comments api

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,25 +4,27 @@ class VideosController < ApplicationController
   #DONE api for video search
   def search
     @video = Video.where("title like ?", params[:title]+"%")
-      #@video = @video.each do |v|
-      #  v.created_at.in_time_zone.to_json = v.created_at.strftime("%d:%m:%y %H:%M:%S")
-      #end
-      respond_with do |format|
-        format.json {render :json => {:video => @video }}
-        #format.json {render :json =>  @video.to_json(:methods => :short_date, :except => [:created_at, :updated_at])}
+    #@video = @video.each do |v|
+    #  v.created_at.in_time_zone.to_json = v.created_at.strftime("%d:%m:%y %H:%M:%S")
+    #end
+    respond_with do |format|
+      format.json {render :json => {:video => @video }}
+      #format.json {render :json =>  @video.to_json(:methods => :short_date, :except => [:created_at, :updated_at])}
 
-      end
+    end
   end
   #DONE api for all comments of particular video
   def show
     if video = Video.find_by_id(params[:video_id])
-      if @comment = video.comments
+      @comment = video.comments
+      if @comment.empty?
         respond_with do |format|
-          format.json {render :json => {:success => true, :video => video, :comments => @comment}}
+          format.json {render :json => {:success => true, :video => video, :comments => "no comments"}}
         end
       else
         respond_with do |format|
-          format.json {render :json => {:success => true, :video => video, :comments => "no comments"}}
+          format.json {render :json => {:success => true, :video => video, :comments => @comment}}
+
         end
       end
     else
